58dc9c43889b335a194c4f0df7706d67aa99decc
Roofius <rufath733@gmail.com>
2025-09-25 01:57:37 +0400

    Create auth0-config.js

---
+++ src/auth0-config.js
@@ -0,0 +1,7 @@
+export const auth0Config = {
+  domain: "dev-h5jv8m8lqvu6dl72.us.auth0.com",
+  clientId: "hPTh5CICNcYJkjWBL6zxNEq5CGIjB6fO",
+  authorizationParams: {
+    redirect_uri: window.location.origin
+  }
+};
